0dd1e16fe19d1119af7741174e951cdadfdff774
vinod jangamsetty <vinodjangamsetty@outlook.com>
2025-01-20 16:33:15 +0530

    testing1

--- clientside/app1/src/app/employee-service.service.ts
+++ clientside/app1/src/app/employee-service.service.ts
@@ -7,27 +7,24 @@ import { Employee } from './employee-list/employee-list.component';
   providedIn: 'root'
 })
 export class EmployeeServiceService {
-private baseUrL="http://localhost:9090/Employee/data"
-  constructor(private http:HttpClient) {
-   }
-   getEmployeeList() : Observable<Employee[]>
-{
-  return this.http.get<Employee[]>(`${this.baseUrL}`)
-}
+  private baseUrL = "http://localhost:9090/Employee/data"
+  constructor(private http: HttpClient) {
+  }
+  getEmployeeList(): Observable<Employee[]> {
+    return this.http.get<Employee[]>(`${this.baseUrL}`)
+  }
 
-createEmployee(employee:Employee)
-{
-  return this.http.post(`${"http://localhost:9090/Employee/datapost"}`,employee)
-}
-getEmployeeByid(id:any):Observable<Employee>
-{
-  return this.http.get<Employee>(`${"http://localhost:9090/Employee/data"}/${id}`);
-}
-updateEmployee(id:any, updatedEmployee: Employee): Observable<Employee> {
-  const url = `${"http://localhost:9090/Employee/update"}/${id}`; // Assuming your API endpoint for updating employee is like /employees/:id
-  return this.http.put<Employee>(url, updatedEmployee);
-}
-DeleteEmployeeById(id:any):Observable<Employee>{
-  return this.http.delete<Employee>(`${"http://localhost:9090/Employee/delete"}/${id}`);
-}
+  createEmployee(employee: Employee) {
+    return this.http.post(`${"http://localhost:9090/Employee/datapost"}`, employee)
+  }
+  getEmployeeByid(id: any): Observable<Employee> {
+    return this.http.get<Employee>(`${"http://localhost:9090/Employee/data"}/${id}`);
+  }
+  updateEmployee(id: any, updatedEmployee: Employee): Observable<Employee> {
+    const url = `${"http://localhost:9090/Employee/update"}/${id}`; // Assuming your API endpoint for updating employee is like /employees/:id
+    return this.http.put<Employee>(url, updatedEmployee);
+  }
+  DeleteEmployeeById(id: any): Observable<Employee> {
+    return this.http.delete<Employee>(`${"http://localhost:9090/Employee/delete"}/${id}`);
+  }
 }
